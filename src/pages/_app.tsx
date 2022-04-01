0b8f0eddef7153281b7a462e6c3407709bb52635
Pedro Coelho <pedrohenriquesc@gmail.com>
2022-04-01 12:01:04 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,30 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import Script from 'next/script'
 
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="description" content="Worldle Flags" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
 
       <ToastContainer
         position="top-center"
